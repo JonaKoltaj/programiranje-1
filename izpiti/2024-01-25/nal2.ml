012103384ff20b926186dff79700a62509a86a8a
Jona Koltaj <jona.koltaj@gmail.com>
2024-08-27 00:09:33 +0200

    Vaje za izpit 3

--- izpiti/2024-01-25/nal2.ml
+++ izpiti/2024-01-25/nal2.ml
@@ -2,6 +2,13 @@ type 'a dict = (string * 'a) list
 type primitive = Bool of bool | Int of int | String of string | Null
 type json = Primitive of primitive | Object of json dict | Array of json list
 
+let reverse list =
+  let rec aux acc = function
+    | [] -> acc
+    | x :: xs -> aux (x::acc) xs
+  in
+  aux [] list
+
 let json_example =
   Object
   [
@@ -35,4 +42,75 @@ let prestej_stevila j =
   in
   aux 0 j
 
-  
+let izloci_nize globina jsn =
+  let rec aux acc i json = match json with
+    |Primitive a ->
+      if i <= 0 then (match a with
+        |String s -> s::acc 
+        |_ -> acc)
+      else acc
+    |Array list -> (match list with
+        |[] -> acc
+        |x::xs -> aux (aux acc (i-1) x) i (Array(xs)))
+    |Object d -> (match d with
+        |[] -> acc
+        |(_,c)::cs -> aux (aux acc (i-1) c) i (Object(cs)))
+  in
+  aux [] globina jsn
+
+let dodaj_predpono str jsn = 
+  let rec aux acc = function
+    |Object d -> (match d with
+      |[] -> acc
+      |(s,c)::ss -> aux ((str ^ s, c)::acc) (Object(ss))
+      )
+    |_ -> acc
+  in
+  Object(reverse(aux [] jsn))
+
+let rec izpisi jsn =
+  let print_list lst =
+    print_string "[";
+    let rec aux = function
+      |[] -> print_endline "]"
+      |[x] -> izpisi x; print_string "]"
+      |x::xs -> izpisi x; print_string ", "; aux xs
+    in
+    aux lst
+  in
+  let print_dict_list lst =
+    print_endline "{";
+    let rec aux = function
+      |[] -> print_endline "}"
+      |[(s,c)] -> print_string ("  " ^ s ^ ": "); izpisi c;print_endline ""; print_endline "}"
+      |(s,c)::cs -> print_string ("  " ^ s ^ ": "); izpisi c;print_endline ","; aux cs
+    in
+    aux lst
+  in
+  match jsn with
+  |Object d -> print_dict_list d
+  |Array list -> print_list list
+  |Primitive a -> (match a with
+    |Bool f -> if f then print_string "true" else print_string "false"
+    |Int i -> print_int i
+    |String s -> print_string s
+    |Null -> print_string "null"
+    )
+
+let type_of_primitive = function (* tle mu je pisalo "and" namesto "let" *)
+  | Bool _ -> "bool"
+  | Int _ -> "int"
+  | String _ -> "string"
+  | Null -> "null"
+let same_type_elements = function (* ne rabi posebej preveriti za enega *)
+  |[] -> true
+  |x::xs -> List.for_all (fun a -> (type_of_primitive a) = (type_of_primitive x)) xs
+let rec je_konsistenten = function
+  | Primitive _ -> true
+  | Object dict -> List.for_all (fun (_, v) -> je_konsistenten v) dict
+  | Array lst -> (match lst with
+    |[] -> true
+    |x::xs -> (match x with
+      |Primitive a -> same_type_elements (x::xs)
+      )
+    )
